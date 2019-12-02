22dd1aaefaf22a5289130665975b387226c04223
JosepHyv <50306337+JosepHyv@users.noreply.github.com>
2019-12-01 20:56:19 -0600

    Update uppercase_linter.py

--- test/uppercase_linter.py
+++ test/uppercase_linter.py
@@ -16,6 +16,7 @@ def main() -> None:
 
     with open(filename, 'w') as output_file:
         output_file.write(contents.upper())
+     cout<<"ya se que no es c++ pero HOLA MUNDO";
 
 
 if __name__ == '__main__':
